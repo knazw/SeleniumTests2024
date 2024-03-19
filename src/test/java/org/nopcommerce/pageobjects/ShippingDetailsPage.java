d8ea528a9b20ce4060ac4b699314075c4eaa0ea8
krzys <krzysie.faketext.nazwisko@gmail.com>
2024-03-19 19:24:32 +0100

    Removing implicit wait from tests.

--- src/test/java/org/nopcommerce/pageobjects/ShippingDetailsPage.java
+++ src/test/java/org/nopcommerce/pageobjects/ShippingDetailsPage.java
@@ -8,14 +8,17 @@ import org.openqa.selenium.support.CacheLookup;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.*;
+import org.slf4j.Logger;
 
 import java.time.Duration;
 import java.util.List;
-import java.util.NoSuchElementException;
 
+import static java.lang.invoke.MethodHandles.lookup;
 import static org.openqa.selenium.support.locators.RelativeLocator.with;
+import static org.slf4j.LoggerFactory.getLogger;
 
 public class ShippingDetailsPage extends ExtendedBasePage {
+    static final Logger log = getLogger(lookup().lookupClass());
 
     @FindBy(id = "CountryId")
     @CacheLookup
@@ -32,6 +35,10 @@ public class ShippingDetailsPage extends ExtendedBasePage {
     @FindBy(xpath = "//button[@class='button-2 apply-shipping-button']")
     @CacheLookup
     WebElement buttonApply;
+    
+    @FindBy(xpath = "//div[@class='estimate-shipping-row-item shipping-item'][contains(text(),'2nd')]")
+    @CacheLookup
+    WebElement shippingBox;
 
     public ShippingDetailsPage(WebDriver driver) {
         super(driver);
@@ -39,9 +46,8 @@ public class ShippingDetailsPage extends ExtendedBasePage {
     }
 
     public ShippingDetailsPage selectCountry(String country) {
-        By byCountry = getByFromElement(this.country);
+        By byCountry = getByFromElement(this.country.toString());
         new WebDriverWait(driver, Duration.ofSeconds(30)).until(ExpectedConditions.elementToBeClickable(byCountry));
-
 
         Select select = new Select(this.country);
         select.selectByVisibleText(country);
@@ -55,19 +61,26 @@ public class ShippingDetailsPage extends ExtendedBasePage {
         return this;
     }
 
-    public ShippingDetailsPage checkRadioButtonShippingMethod(String shippingMethodName) {
-        Wait<WebDriver> fluentWait = new FluentWait<>(driver)
-                .pollingEvery(Duration.ofSeconds(1))
-                .withTimeout(Duration.ofSeconds(10))
-                .ignoring(NoSuchElementException.class);
+    public ShippingDetailsPage shippingMethodAvailable() {
+//        takeSnapShot(driver, "C:\\src\\selenium\\_screenshots\\shippingMethodAvailable-start.png");
+        clickWithWait(shippingBox,10);
 
+        return this;
+
+    }
+
+    public ShippingDetailsPage checkRadioButtonShippingMethod(String shippingMethodName) {
+        // wait until whole item is displayed
+        By byShippingBox = getByFromElement(shippingBox.toString());
+        findWithWait(byShippingBox, 10);
+
+        // find reference to specific item
         WebElement choosenMethod = this.shippingMethodName.stream()
                 .filter(el -> el.getText().equals(shippingMethodName))
                 .findFirst().orElse(null);
 
-        WebElement element = fluentWait.until(ExpectedConditions.elementToBeClickable(choosenMethod));
         WebElement radioButton = driver.findElement(with(By.tagName("label"))
-                .toLeftOf(element));
+                .toLeftOf(choosenMethod));
         radioButton.click();
 
         return this;
