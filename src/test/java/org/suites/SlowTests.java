b36c6573d3463a91520b09e2d8421808b2522dfa
krzys <krzysie.faketext.nazwisko@gmail.com>
2024-04-02 16:47:51 +0200

    Disable test suites, disable running tests in docker

--- src/test/java/org/suites/SlowTests.java
+++ src/test/java/org/suites/SlowTests.java
@@ -4,8 +4,8 @@ import org.junit.platform.suite.api.IncludeTags;
 import org.junit.platform.suite.api.SelectPackages;
 import org.junit.platform.suite.api.Suite;
 
-@Suite
-@SelectPackages("org")
-@IncludeTags("Slow")
+//@Suite
+//@SelectPackages("org")
+//@IncludeTags("Slow")
 public class SlowTests {
 }
